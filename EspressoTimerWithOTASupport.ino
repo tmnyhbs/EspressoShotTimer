2f771c6bdbdeb27c9fa4483626f84b2fc12343e6
Dave in Chicago <1204794+tmnyhbs@users.noreply.github.com>
2024-08-24 08:54:40 -0500

    Set time zone to GMT by removing GMTOffsetsec

--- EspressoTimerWithOTASupport.ino
+++ EspressoTimerWithOTASupport.ino
@@ -14,7 +14,7 @@ const char *ssid = "...";
 const char *password = "...";
 
 const char* ntpServer = "pool.ntp.org";
-const long  gmtOffset_sec = -21600;
+const long  gmtOffset_sec = 0;
 const int   daylightOffset_sec = 3600;
 
 #define SCREEN_WIDTH 128
